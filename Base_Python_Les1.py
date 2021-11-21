f1da0645cffb9758036b082af544e1fd9a7b9c02
AvokrichA <leysanspb@mail.ru>
2021-11-21 12:23:46 +0300

    bvnmh

--- Base_Python_Les1.py
+++ Base_Python_Les1.py
@@ -26,18 +26,19 @@
 # Пример: Вася Пупкин, 41 год, вес 121 - следует обратится к врачу!
 # Пример: Вася Пупкин, 41 год, вес 49 - следует обратится к врачу!
 #
-# name=input('Введите имя:')
-# syrname=input('Введите фамилию:')
-# age=int(input('Введите возраст:'))
-# weight=int(input('Введите вес:'))
-#
-# if age<=30 and (weight<=120 and weight>=50):
-#     print(name, syrname, age,'год', weight, 'кг','-Хорошее состояние')
-# elif age>30 and age<=40 and (weight<50 or weight>120):
-#     print(name, syrname, age,'год', weight, 'кг' '-Займитесь собой')
-# elif age>40 and (weight<50 or weight>120):
-#     print(name, syrname, age,'год', weight, 'кг', '-Обратитесь к врачу')
-#
-# else: "Все же сходите к врачу"   (Примечание: можно написать это строчку, но по заданию она не требуется)
+name=input('Введите имя:')
+syrname=input('Введите фамилию:')
+age=int(input('Введите возраст:'))
+weight=int(input('Введите вес:'))
+
+if age<=30 and (weight<=120 and weight>=50):
+    print(name, syrname, age,'год', weight, 'кг','-Хорошее состояние')
+elif age>30 and age<=40 and (weight<50 or weight>120):
+    print(name, syrname, age,'год', weight, 'кг' '-Займитесь собой')
+elif age>40 and (weight<50 or weight>120):
+    print(name, syrname, age,'год', weight, 'кг', '-Обратитесь к врачу')
+
+else: "Все же сходите к врачу"
+# (Примечание: можно написать это строчку, но по заданию она не требуется)
 
 
